46dca44382a13f111712708c41a2bb5908cb154d
Lillian Zhang <Lillian Zhang>
2016-09-21 04:13:30 -0400

    added controller for pulling up login screen

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,25 @@
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class Controller{
+    @FXML
+    private void handleLoginButtonAction(ActionEvent event) {
+        System.out.println("clicked");
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
+            Stage loginStage = new Stage();
+            loginStage.setTitle("Login Screen");
+            loginStage.setScene(new Scene(root,600,400));
+            loginStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
