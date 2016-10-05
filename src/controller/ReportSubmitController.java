39581bdc3d58c783daee1ba8ba1694d2c4dc8b11
Jon Jiang <jon.jiang1@gmail.com>
2016-10-05 01:08:07 -0400

    Merge M6 with master, added cancel button functionality to submitting water report.

--- src/controller/ReportSubmitController.java
+++ src/controller/ReportSubmitController.java
@@ -1,8 +1,13 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Created by Jonathan on 10/3/2016.
@@ -23,4 +28,21 @@ public class ReportSubmitController {
         thisStage.hide();
     }
 
+    @FXML
+    private void handleCancelPressed() {
+        Stage thisStage = (Stage) headingText.getScene().getWindow();
+        thisStage.close();
+        thisStage.hide();
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/LandingScreen.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+            Stage profileStage = new Stage();
+            ProfileScreenController psc = fxmlLoader.<ProfileScreenController>getController();
+            profileStage.setTitle("Landing Screen");
+            profileStage.setScene(new Scene(root,600,400));
+            profileStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
