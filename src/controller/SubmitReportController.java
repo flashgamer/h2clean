883843feda479ee7b823598b2b0f9e9f507caa5c
jialee <alicia15kc@gmail.com>
2016-10-16 01:31:06 -0400

    Started work on backend code to support checking if user is authorized to submit a type of report.

--- src/controller/SubmitReportController.java
+++ src/controller/SubmitReportController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import model.Account;
@@ -21,7 +23,21 @@ public class SubmitReportController {
     @FXML
     Label headingText;
 
+    @FXML
+    ComboBox typeReport;
+
     private String userKey;
+
+    private Stage _submitReportPopupStage;
+
+    /**
+     * Sets the stage of the dialog.
+     *
+     * @param submitReportPopupStage the stage of the login
+     */
+    public void setLoginStage(Stage submitReportPopupStage) {
+        _submitReportPopupStage =
+            submitReportPopupStage; }
 
     /**
      * Called when Submit New Report Button pressed.
@@ -61,24 +77,52 @@ public class SubmitReportController {
      */
     @FXML
     private void handleSubmitButtonAction(ActionEvent event) {
+        if (isUserValid().equals("error")) {
+
+            //show error message
+            String errorMessage = "You are not authorized to view this report" +
+                    ".\n";
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.initOwner(_submitReportPopupStage);
+            alert.setTitle("Invalid Login");
+            alert.setHeaderText("Please try again with the correct login details.");
+            alert.setContentText(errorMessage);
+
+            alert.showAndWait();
+        } else if (isUserValid().equals("noneSelected")) {
+
+        } else if (isUserValid().equals("waterSourceReport")) {
+
+        } else if (isUserValid().equals("waterPurityReport")) {
+
+        }
+    }
+
+    private String isUserValid() {
         Account account = database.get(userKey);
         String accType = account.getAccountType();
 
-        if (isReportValid()) {
-            String errorMessage = "";
-        }
-
-        //Checks if user is authorized to submit a type of report, error
+        // Checks if user is authorized to submit a type of report, error
         // message pops up if otherwise.
-
         if (accType.equals("User")) {
-            if ()
-
+            if (typeReport.getValue().equals("waterSourceReport")) {
+                return "waterSourceReport";
+            } else if (typeReport.getValue().equals("waterPurityReport")) {
+                return "error";
+            }
         } else if (accType.equals("Worker")) {
-
+            if (typeReport.getValue().equals("waterSourceReport")) {
+                return "waterSourceReportScreen";
+            } else if (typeReport.getValue().equals("waterPurityReport")) {
+                return "waterPurityReportScreen";
+            }
         } else if (accType.equals("Manager")) {
-
+            if (typeReport.getValue().equals("waterSourceReport")) {
+                return "waterSourceReportScreen";
+            } else if (typeReport.getValue().equals("waterPurityReport")) {
+                return "waterPurityReportScreen";
+            }
         }
-
+        return "noneSelected";
     }
 }
