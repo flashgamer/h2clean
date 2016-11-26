bb9b032f4849d384329a3744c6816bfa34fb3afb
Lillian Zhang <Lillian Zhang>
2016-11-26 18:08:56 -0500

    sql stuff i cri

---
+++ src/model/Sample.java
@@ -0,0 +1,54 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Sample
+{
+    public static void main(String[] args)
+    {
+        Connection connection = null;
+        try
+        {
+            // create a database connection
+            connection = DriverManager.getConnection("jdbc:sqlite:accountsDB.db");
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+            statement.executeUpdate("drop table if exists accountsDB");
+            statement.executeUpdate("create table accountsDB (username string, password string, accountType string, " +
+                    "title string, firstName string, lastName string, email string, address string)");
+//            statement.executeUpdate("insert into person values(1, 'leo')");
+//            statement.executeUpdate("insert into person values(2, 'yui')");
+            ResultSet rs = statement.executeQuery("select * from person where id = 1");
+            while(rs.next())
+            {
+                // read the result set
+                System.out.println("name = " + rs.getString("name"));
+                System.out.println("id = " + rs.getInt("id"));
+            }
+        }
+        catch(SQLException e)
+        {
+            // if the error message is "out of memory",
+            // it probably means no database file is found
+            System.err.println(e.getMessage());
+        }
+        finally
+        {
+            try
+            {
+                if(connection != null)
+                    connection.close();
+            }
+            catch(SQLException e)
+            {
+                // connection close failed.
+                System.err.println(e);
+            }
+        }
+    }
+}
